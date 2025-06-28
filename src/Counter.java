05caffd71c45e7865d719e8ed756c2978a94f2ed
vlcdck <pridatchenkovladislav@gmail.com>
2025-06-28 12:39:42 +0300

    multithreading

---
+++ src/Counter.java
@@ -0,0 +1,13 @@
+public class Counter {
+
+    private int counter = 0;
+
+    public void increment() {
+        counter++;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,47 @@
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+//        Runnable runnable = () -> {
+//            for (int i = 0; i < 5; i++) {
+//                System.out.println("Number: " + i + " from thread: " + Thread.currentThread().getName());
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (InterruptedException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            }
+//        };
+//
+//        Thread thread1 = new Thread(runnable);
+//        Thread thread2 = new Thread(runnable);
+//
+//        thread1.start();
+//        thread2.start();
+//
+//        thread1.join();
+//        thread2.join();
+
+        ExecutorService executorService = Executors.newFixedThreadPool(100);
+
+        for (int i = 0; i < 10000; i++) {
+            int taskNumber = i;
+            executorService.submit(() -> {
+                System.out.println("Task № " + taskNumber + "run by thread: " + Thread.currentThread().getName());
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            });
+        }
+
+        executorService.shutdown();
+        executorService.awaitTermination(1, TimeUnit.MINUTES);
+        System.out.println("---");
 
     }
 }
